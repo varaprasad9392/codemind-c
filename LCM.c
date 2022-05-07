cfa27572858762d81031f0f45620051319038dca
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-05-07 12:29:10 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for(i=1;i<=a*b;i++)     {         if(i%a==0 && i%b==0)         {             printf("%d",i);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -2,12 +2,12 @@
 int main()
 {
     int a,b,i;
-    scanf("%d %d",&a,&b);
+    scanf("%d%d",&a,&b);
     for(i=1;i<=a*b;i++)
     {
         if(i%a==0 && i%b==0)
         {
-            printf("%d ",i);
+            printf("%d",i);
             break;
         }
     }
