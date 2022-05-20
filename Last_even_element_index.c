59a3815663e8cc854ff8a00550e43ee87bc7fe58
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-05-20 19:49:40 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int index=0;     for(i=0;i<n;i++)     if(arr[i]%2==0)     {        index=i;     }     printf("%d",index);
    }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int index=0;
+    for(i=0;i<n;i++)
+    if(arr[i]%2==0)
+    {
+       index=i; 
+    }
+    printf("%d",index);
+    
+}
