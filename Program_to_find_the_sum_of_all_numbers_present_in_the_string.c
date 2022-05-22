c5af92313fc018e3fb6779d158b4a8db41d344f7
varaprasad9392 <97425886+varaprasad9392@users.noreply.github.com>
2022-05-22 21:36:13 +0530

    #include<stdio.h> int main() {     char str[1000];     scanf("%[^ ]s",str);     int sum=0,i;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=49&&str[i]<=58)         {             sum=sum+str[i]-48;         }     }     printf("%d",sum); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[1000];
+    scanf("%[^
+]s",str);
+    int sum=0,i;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=49&&str[i]<=58)
+        {
+            sum=sum+str[i]-48;
+        }
+    }
+    printf("%d",sum);
+}
